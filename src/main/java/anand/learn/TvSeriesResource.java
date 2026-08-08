a6dd1e3494c486900dc084584ac714d275f59fde
anandkumar-patel <kumar.akpatel@gmail.com>
2026-08-08 11:55:09 +0530

    added 4 annotations

--- src/main/java/anand/learn/TvSeriesResource.java
+++ src/main/java/anand/learn/TvSeriesResource.java
@@ -1,5 +1,6 @@
 package anand.learn;
 
+import io.smallrye.faulttolerance.api.RateLimit;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -8,9 +9,12 @@ import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.temporal.ChronoUnit;
 
 @Path("/tvseries")
 public class TvSeriesResource {
@@ -22,24 +26,12 @@ public class TvSeriesResource {
 
     @GET
     @Path("/{id}")
-    @Fallback(
-        fallbackMethod = "getTvSeriesByIdFallback",
-            applyOn = {Exception.class},
-            skipOn = {}
-
-    )
-    @Retry(
-        retryOn = {Exception.class},
-        maxRetries = 3,
-        maxDuration = 10000,
-        abortOn = {}, delay = 1000
-    )
-    @CircuitBreaker(
-        requestVolumeThreshold=4,
-        failureRatio=0.6,
-        successThreshold=2,
-        failOn = {Exception.class},
-        skipOn = {}
+    @Timeout(unit = ChronoUnit.SECONDS, value = 2)
+    @Retry(delayUnit = ChronoUnit.SECONDS, maxRetries = 2, delay = 1)
+    @Fallback(fallbackMethod = "getTvSeriesByIdFallback")
+    @CircuitBreaker(delayUnit = ChronoUnit.SECONDS, requestVolumeThreshold = 4,
+            failureRatio = .75, delay = 3,
+            successThreshold = 2
     )
     public Response getTvSeriesById(@PathParam("id") int id) {
         log.info("calling the tv series service from my app...");
